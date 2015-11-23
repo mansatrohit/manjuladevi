5ea3eb1f9329f99c2ba3c13f2159d152987d605c
mansatrohit <satishchandarg@gmai.com>
2015-11-23 13:11:01 +0530

    good

---
+++ assign1.java
@@ -0,0 +1,13 @@
+public class sum {
+  public static void main(String args[]){
+  /* 
+  SUM OF TWO NUMBERS
+  */
+  int sum,a=5;
+  float b = 5.5;
+  sum = a+b;
+  system.out.println("sum of two numbers");
+  system.out.println(a+"+"+b"="sum);
+  }
+  }
+  
